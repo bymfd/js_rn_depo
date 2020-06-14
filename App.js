1bb68b28344ea654918fcb3896cab9b0f7ae348a
bymfd <frukolosko@gmail.com>
2020-06-14 15:37:44 +0300

    basic todo app with rn

--- App.js
+++ App.js
@@ -1,36 +1,148 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  TextInput,
+    ScrollView,
+} from 'react-native'
+// import {ScrollView} from "react-native-web";
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
 
-const Stack = createStackNavigator();
+class App extends Component {
 
-export default function App(props) {
-  const isLoadingComplete = useCachedResources();
+  state = {
+      task:'',
+      tasklist:[],
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+
+
+  }
+    handletask = (text) => {
+        this.setState({task: text })
+
+    }
+    handlebuton=(kim)=>
+    {
+
+            this.state.tasklist.push(this.state.task);
+             this.setState({task: '' })
+
+
+    }
+    renderitem=(item) => {
+
+
+
+
+          return (<View style={styles.kutu}>
+
+              <Text key={this.state.tasklist.indexOf(item).toString()} style={styles.yazi}>
+                  {
+
+                      item
+
+                  }
+              </Text>
+
+          </View>)
+
+
+
+
+    }
+
+
+
+
+ render() {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
+        //main
+        <View style={{backgroundColor:"#BDBDBD",flex:1}}>
+        {/*head */}
+        <View style={styles.head} >
+
+          {/*  input box */}
+          <View  style={styles.header} >
+              <TextInput style={styles.headtxtinput}
+                         key={"txt"}
+                         value={this.state.task}
+                         placeholder = "Eklemek istediğin herşey"
+                         autoCapitalize = "none"
+                         onChangeText = {text=>this.handletask(text) }
+
+              />
+
+          </View>
+
+        {/*button add */}
+        <TouchableOpacity
+            key={"buton"}
+            style={styles.buton}
+            onPress={()=>this.handlebuton()}
+        >
+         <Text>Ekle</Text>
+        </TouchableOpacity>
+
+
+        </View>
+        {/*end to header */}
+
+
+            <View style={styles.hr}/>
+
+
+
+            <View style={{backgroundColor:"#BDBDBD"}}>
+                <ScrollView style={{backgroundColor:"#BDBDBD"}} >
+                    {
+                        this.state.tasklist.map((item)=>this.renderitem(item))
+
+                    }
+
+                    <View style={styles.hr}/>
+                    <View style={{height:135}}/>
+                </ScrollView>
+
+
+            </View>
+
+
+        </View>
+
+
+    )
   }
 }
 
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+
+    header:{ margin:5,flex:1,backgroundColor:"#BDBDBD",justifyContent:"center" },
+headtxtinput:{ backgroundColor:"#9E9E9E",height:40 ,borderRadius:5,textAlign:"center"},
+head:{paddingTop:21,flexDirection:"row",backgroundColor:"#BDBDBD",height:120, },
+yazi:{fontSize:16,paddingLeft:10,color:"#F5F5F5",},
+kutu:{ backgroundColor:"#757575",alignItems:"center", height:75,borderRadius:15,margin:3,flexDirection:"row",},
+
+hr:{backgroundColor:"gray",height:2,margin:5},
+
+buton:
+{margin:5,
+    minWidth:75,
+    backgroundColor:"#607D8B",
+    justifyContent:"center",
+    alignItems:"center",
+    borderWidth:0,
+    borderRadius:20,
+
+},
+
+
+
+})
+
+export default App;
+
